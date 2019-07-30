faf28a49fdc0191dba5abcdabcbcd4260e496ce1
Stu Freen <stuart.freen@rangle.io>
2019-07-30 11:19:52 -0400

    Initialize repo

---
+++ index.js
@@ -0,0 +1,115 @@
+import * as randomNormal from 'random-normal';
+
+const NUM_PARTICLES = 600;
+const PARTICLE_SIZE = 0.4;
+const SPEED = 20000;
+
+let particles = [];
+
+function rand(low, high) {
+  return Math.random() * (high - low) + low;
+}
+
+function createParticle(canvas) {
+  const colour = {
+    r: 255,
+    g: randomNormal({ mean: 125, dev: 20 }),
+    b: 50,
+    a: rand(0, 1),
+  };
+  return {
+    x: -2,
+    y: -2,
+    diameter: Math.max(0, randomNormal({ mean: PARTICLE_SIZE, dev: PARTICLE_SIZE / 2 })),
+    duration: randomNormal({ mean: SPEED, dev: SPEED * 0.1 }),
+    amplitude: randomNormal({ mean: 16, dev: 2 }),
+    offsetY: randomNormal({ mean: 0, dev: 10 }),
+    arc: randomNormal({ mean: Math.PI * 2, dev: 0.1 }),
+    startTime: performance.now() - rand(0, SPEED),
+    colour: `rgba(${colour.r}, ${colour.g}, ${colour.b}, ${colour.a})`,
+  }
+}
+
+function moveParticle(particle, canvas, time) {
+  const progress = ((time - particle.startTime) % particle.duration) / particle.duration;
+  return {
+    ...particle,
+    x: progress,
+    y: ((Math.sin(progress * particle.arc) * particle.amplitude) + particle.offsetY),
+  };
+}
+
+function drawParticle(particle, canvas, ctx) {
+  canvas = document.getElementById('particle-canvas');
+  const vh = canvas.height / 100;
+
+  ctx.fillStyle = particle.colour;
+  ctx.beginPath();
+  ctx.ellipse(
+    particle.x * canvas.width,
+    particle.y * vh + (canvas.height / 2),
+    particle.diameter * vh,
+    particle.diameter * vh,
+    0,
+    0,
+    2 * Math.PI
+  );
+  ctx.fill();
+}
+
+function draw(time, canvas, ctx) {
+  // Move particles
+  particles.forEach((particle, index) => {
+    particles[index] = moveParticle(particle, canvas, time);
+  })
+
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw the particles
+  particles.forEach((particle) => {
+    drawParticle(particle, canvas, ctx);
+  })
+
+  // Schedule next frame
+  requestAnimationFrame((time) => draw(time, canvas, ctx));
+}
+
+function initializeCanvas() {
+  let canvas = document.getElementById('particle-canvas');
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+  let ctx = canvas.getContext("2d");
+
+  window.addEventListener('resize', () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+    ctx = canvas.getContext("2d");
+  })
+
+  return [canvas, ctx];
+}
+
+function startAnimation() {
+  const [canvas, ctx] = initializeCanvas();
+
+  // Create a bunch of particles
+  for (let i = 0; i < NUM_PARTICLES; i++) {
+    particles.push(createParticle(canvas));
+  }
+
+  console.table(particles);
+
+  requestAnimationFrame((time) => draw(time, canvas, ctx));
+};
+
+// Start animation when document is loaded
+(function () {
+  if (document.readystate !== 'loading') {
+    startAnimation();
+  } else {
+    document.addEventListener('DOMContentLoaded', () => {
+      startAnimation();
+    })
+  }
+}());
